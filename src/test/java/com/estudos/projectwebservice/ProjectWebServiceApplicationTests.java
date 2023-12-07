3a7f0dd027dea26482bdde19b12c105e49cf571c
Graciele <oliveiragracie@gmail.com>
2023-12-07 18:59:55 -0300

    Project created

---
+++ src/test/java/com/estudos/projectwebservice/ProjectWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.projectwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
