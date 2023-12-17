dfc52f3cec878eb78c59c83b18b7cc54de611b6a
Graciele <oliveiragracie@gmail.com>
2023-12-17 11:11:51 -0300

    Service layer, component registration

--- src/main/java/com/estudos/projectwebservice/repositories/UserRepository.java
+++ src/main/java/com/estudos/projectwebservice/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.estudos.projectwebservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.estudos.projectwebservice.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
---
+++ src/main/java/com/estudos/projectwebservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.estudos.projectwebservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudos.projectwebservice.entities.User;
+import com.estudos.projectwebservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> optional = repository.findById(id);
+		return optional.get();		
+	}
+}
