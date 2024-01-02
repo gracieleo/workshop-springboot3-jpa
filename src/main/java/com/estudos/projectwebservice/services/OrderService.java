f282abdb140099c177e56b7f39284607260dd475
Graciele <oliveiragracie@gmail.com>
2024-01-02 20:21:17 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/estudos/projectwebservice/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.estudos.projectwebservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudos.projectwebservice.entities.Order;
+import com.estudos.projectwebservice.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> optional = repository.findById(id);
+		return optional.get();		
+	}
+}
