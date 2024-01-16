f890b1aeb09dd9be5624fccb56e586e15b88b160
Graciele <oliveiragracie@gmail.com>
2024-01-16 20:11:19 -0300

    User update

--- src/main/java/com/estudos/projectwebservice/services/UserService.java
+++ src/main/java/com/estudos/projectwebservice/services/UserService.java
@@ -31,4 +31,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		// ainda não passa no banco de dados, prepara/monitora o objeto
+		//antes era getOne
+		User entity = repository.getReferenceById(id); 
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		// atualiza entity com base no que veio no obj
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}	
 }
